e9d7f245d962b5133e83e214f2057d044b5f2aaf
Andres Fernandez <andresfernandezflores19@gmail.com>
2025-04-14 01:26:10 +0300

    Documented class

--- fusion/include/algorithms/sequential_engine.hpp
+++ fusion/include/algorithms/sequential_engine.hpp
@@ -13,10 +13,41 @@ namespace fusion
 class SequentialEngine : public SimulationEngine
 {
 public:
+    /**
+     * @brief Construct a new Sequential Engine object
+     * 
+     * @param config  Configuration for the simulation
+     */
     explicit SequentialEngine(const SimulationConfig& config);
+
+    /**
+     * @brief Register an entity to the simulation
+     * 
+     * @param entity Entity to register
+     */
     void registerEntity(std::shared_ptr<Entity> entity) override;
+
+    /**
+     * @brief Initialize the simulation
+     * 
+     */
     void initialize() override;
+
+    /**
+     * @brief Run the simulation until the stop conditions are met.
+     * 
+     * the stop condition depends on the time limit set in the configurations, 
+     * if the time limit is reached no more events will be generated which will lead for the simulation to stop
+     * 
+     * @return SimulationStats Statistics about the simulation run
+     */
     SimulationStats run() override;
+
+    /**
+     * @brief Get the Current simulation time
+     * 
+     * @return double The current simulation time
+     */
     double getCurrentTime() const override;
 
 private:
